aaacb7188d2b8ecae15b3e5c6ac9bfa4f0dd8e42
minoharakouta <minoku0794@gmail.com>
2021-10-17 20:36:04 +0900

    投稿用テーブル

---
+++ src/todo_input.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include("DB_Function.php");
+check_session_id();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Posting（入力画面）</title>
+</head>
+
+<body>
+  <form action="todo_create.php" method="POST">
+    <fieldset>
+      <legend>Posting（入力画面）</legend>
+      <a href="todo_read.php">みんなの投稿</a>
+      <a href="todo_logout.php">ログアウト</a>
+      <input type="hidden" name="post[post_user_id"] value="1">
+      <div>
+        件名: <input type="text" name="post[post_title]" size=30>
+      </div>
+      <div>
+        内容: <textarea name = "post[post_content]" rows = 10 cols = 40>
+            </textarea><br>
+      </div>
+      <div>
+        <button>Posting</button>
+      </div>
+    </fieldset>
+  </form>
+
+</body>
+
+</html>
