0ceb20210e33cd16e8af099af2179b56702b7916
Inoue Masato <masa_0609@icloud.com>
2021-10-19 17:32:11 +0900

    change 太郎資料

--- src/read.php
+++ src/read.php
@@ -1,47 +1,47 @@
 <?php
 
-// require("DB_Function.php");
+require("DB_Function.php");
 
-$pdo = connect_to_db();
+// $pdo = connect_to_db();
 
-$sql = "SELECT post_title, post_user_id, post_created FROM post JOIN user ON post.post_user_id = user.user_id";
+// $sql = "SELECT post_title, post_user_id, post_created FROM post JOIN user ON post.post_user_id = user.user_id";
 
-$stmt = $pdo->prepare($sql);
-$status = $stmt->execute();
+// $stmt = $pdo->prepare($sql);
+// $status = $stmt->execute();
 
-if ($status == false) {
-  $error = $stmt->errorInfo();
-  echo json_encode(["error_msg" => "{$error[2]}"]);
-  exit();
-} else {
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  $output = "";
-  foreach ($result as $record) {
-    $output .= "
-      <tr>
-        <td>{$record["post_title"]}</td>
-        <td>{$record["user_name"]}</td>
-        <td>{$recode["post_created"]}</td>
-        <td><a href='like_create.php?user_id={$user_id}&todo_id={$record["id"]}'>like</a></td>
-        // <td><a href='todo_edit.php?id={$record["id"]}'>edit</a></td>
-        // <td><a href='todo_delete.php?id={$record["id"]}'>delete</a></td>
-      </tr>
-    ";
-  }
-}
-
-// $act = DB_Function::creat()->connect("team_h",)->toSELECT("post",$columns = ["post_user_id, post_id"])
-// ->toEXECUTE();
-
-// $all = $act->fetch(PDO::FETCH_ASSOC);
-
-// foreach($all as $row){
-//     echo "{$row}<br>";
+// if ($status == false) {
+//   $error = $stmt->errorInfo();
+//   echo json_encode(["error_msg" => "{$error[2]}"]);
+//   exit();
+// } else {
+//   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//   $output = "";
+//   foreach ($result as $record) {
+//     $output .= "
+//       <tr>
+//         <td>{$record["post_title"]}</td>
+//         <td>{$record["user_name"]}</td>
+//         <td>{$recode["post_created"]}</td>
+//         <td><a href='like_create.php?user_id={$user_id}&todo_id={$record["id"]}'>like</a></td>
+//         // <td><a href='todo_edit.php?id={$record["id"]}'>edit</a></td>
+//         // <td><a href='todo_delete.php?id={$record["id"]}'>delete</a></td>
+//       </tr>
+//     ";
+//   }
 // }
-// // if(status == false){
-// //     $error = $stmt->errorInfo();
-// //     echo json_encode(["error_msg" => "{$error"])
-// // }
+
+$act = DB_Function::creat()->connect("team_h",)->toSELECT("post",$columns = ["post_user_id, post_id"])
+->toEXECUTE();
+
+$all = $act->fetch(PDO::FETCH_ASSOC);
+
+foreach($all as $row){
+    echo "{$row}<br>";
+}
+// if(status == false){
+//     $error = $stmt->errorInfo();
+//     echo json_encode(["error_msg" => "{$error"])
+// }
 
 ?>
 
@@ -65,15 +65,15 @@ if ($status == false) {
                 <th>タイトル</th>
                 <th>名前</th>
                 <th>更新日</th>
-                <!-- <form action="like.php" method="POST">
+                <form action="like.php" method="POST">
                     <input type = "hidden" name="user_id" value="<?= $all["user_id"] ?>">;
                     <input type = "hidden" name="user_id" value="<?= $all["user_id"] ?>">;
                     <button>like</button>
-                </form> -->
+                </form>
             </tr>
         </thead>
         <tbody>
-            <?= $output ?>
+            <?= $all ?>
         </tbody>
     </table>
 </body>
