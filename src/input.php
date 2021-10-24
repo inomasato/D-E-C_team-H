a6933aeef8c84052b1f9c85f1123d734609c7456
minoharakouta <minoku0794@gmail.com>
2021-10-24 12:39:39 +0900

    投稿用テーブル

--- src/input.php
+++ src/input.php
@@ -17,23 +17,23 @@ include("DB_Function.php");
 <body>
   <nav>
   <form action="create.php" method="POST">
-    <div>
+    
       <fieldset>
         <legend>Posting（入力画面）</legend>
         <a href="read.php">みんなの投稿</a>
         <a href="logout.php">ログアウト</a>
         <input type="hidden" name="post[post_user_id]" value="1">
         <div>
-          件名: <input type="text" name="post[post_title]" size=40>
+          件名: <input type="text" name="post[post_title]" >
         </div>
         <div>
-          内容: <textarea name = "post[post_content]" rows = 10 cols = 40></textarea><br>
+          内容: <textarea name = "post[post_content]" ></textarea><br>
         </div>
         <div>
           <button>Posting</button>
         </div>
       </fieldset>
-    </div>
+    
   </form>
 </nav>
 
