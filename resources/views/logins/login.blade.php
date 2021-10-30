7ec58883d5e3e90bad06e97fb3aeae7b2a57bf1f
Inoue Masato <masa_0609@icloud.com>
2021-10-30 20:02:44 +0900

    change follower

--- resources/views/logins/login.blade.php
+++ resources/views/logins/login.blade.php
@@ -15,7 +15,8 @@
     </header>
     <div class='content'>    
         <div class="form_content">
-            <form action="login" method="POST">
+            {{-- <form action="login" method="POST"> --}}
+                <form action="login" method="POST">
                 @csrf
                 <div class="form_text">ログインID</div>
                 <input type="text" name="loginId">
