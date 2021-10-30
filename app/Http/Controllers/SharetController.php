e86051baad5937b097718cf47110bf034c697f69
Aso2001385 <2001385@s.asojuku.ac.jp>
2021-10-31 00:42:09 +0900

    likeJudge成功

--- app/Http/Controllers/SharetController.php
+++ app/Http/Controllers/SharetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Traits\EnumeratesValues;
 use App\Models\Like;
 use App\Models\Tweet;
 
@@ -25,11 +26,12 @@ class SharetController extends Controller
        
         $judge_cnt = 0;
 
+
         foreach($likes as $like){
             foreach($tweets['tweets'] as $tweet){
-                $like_judge[$judge_cnt] = ['like_judge' => false];
+                $like_judge[$judge_cnt] = ['like_judge' => 0];
                 if($like->like_tweet_id == $tweet->tweet_id){
-                    $like_judge[$judge_cnt] = ['like_judge' => true];
+                    $like_judge[$judge_cnt] = ['like_judge' => 1];
                     break;
                 }
             }
@@ -37,9 +39,31 @@ class SharetController extends Controller
             unset($tweet);
         }
 
+        
+        $convert = [];
+        $i = 0;
+        foreach($tweets["tweets"] as $tweet){
+             $convert[$i++] = (array)$tweet;
+        }
 
+        $items = $like_judge;
+        $items[] = $convert;
+        $output = "";
+
+        foreach($items as $item){
+            print_r($item);
+            exit();
+        }
+       
+
+        foreach($items as $item){
+            $output .= "{$item->tweet_content}:{$item->like_judge}";
+            echo $output."<br>";
+        }
+        
+        exit();
      
-        return view('sharet.index',['user_data',$user_data]);
+        return view('sharet.index',['items',$items]);
 
     }
 }
