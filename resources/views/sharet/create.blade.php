cdd0cf4cb4b72a56de2ceb45e9c01c2067b1672c
minoharakouta <minoku0794@gmail.com>
2021-10-31 04:54:52 +0900

    作成関連

--- resources/views/sharet/create.blade.php
+++ resources/views/sharet/create.blade.php
@@ -11,13 +11,13 @@
         <form method="post" actions="/sharet/create">
           @csrf
           <textarea class= "create" name="sharet" >投稿しよう</textarea><br>
-        </form>
         <input type="radio" class= "button1" name="feel">
         <div class="positive" >ポジティブ</div> 
         <input type="radio" class= "button2" name="feel">
         <div class="negative">ネガティブ</div> 
         <input type="submit"  class="do" value="投稿する">
         <input type="submit" class="undo" value="投稿しない"> 
+        </form>
     </div>
 </div>
 @endsection
