b0a431bc80f56b484e4ae6efb6f1c2fd87bd18eb
minoharakouta <minoku0794@gmail.com>
2021-10-30 19:52:54 +0900

    作成

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class CreateControllerr extends Controller
+class CreateController extends Controller
 {
     public function index(Request $request){
         $user_data = $request->session()->get('user_data');
         return view ('sharet/create',$user_data);
     }
-    public function create(Request $request){
-        $param =[
-            'tweet_content'=>$request->tweet_content,
-        ];
-        DB::insert('insert into tweet (tweet_content,) values (:tweet_content)', $param);
-        return redirect();
-    }
+    
+    // public function create(Request $request){
+    //     $param =[
+    //         'tweet_content'=>$request->tweet_content,
+    //     ];
+    //     DB::insert('insert into tweet (tweet_content) values (:tweet_content)', $param);
+    //     return redirect();
+    // }
 }
